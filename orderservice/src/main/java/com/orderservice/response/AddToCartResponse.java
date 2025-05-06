dd9b66637c82e0b37094c54beb6331516213c0e2
Quyen <bntquyenn@gmail.com>
2025-05-06 23:36:25 +0700

    Add cart component

---
+++ orderservice/src/main/java/com/orderservice/response/AddToCartResponse.java
@@ -0,0 +1,13 @@
+package com.orderservice.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse {
+    String message;
+
+}
---
+++ orderservice/src/main/java/com/orderservice/response/UpdateCartResponse.java
@@ -0,0 +1,2 @@
+package com.orderservice.response;public class UpdateCartResponse {
+}
